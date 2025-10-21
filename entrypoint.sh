54227a88a9c66b77e8aea584e9a856c715146cf2
select766 <select766@outlook.jp>
2025-10-21 09:43:02 +0900

    support INPUT_FORMAT

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,22 @@
 [ -z "${INPUT_CONFIG_FILE}" ] && { echo "Need to set CONFIG_FILE. Default is 'config.yml'"; exit 1; }
 [ -z "${INPUT_FORMAT}" ] && { echo "Need to set INPUT_FORMAT. Default is 'PDF'"; exit 1; }
 
-# TODO Enable env paramaters that only INPUT_TARGET_DIR works well.
+case "$INPUT_FORMAT" in
+  "PDF")
+    NPM_RUN_FORMAT=pdf
+    ;;
+  "EPUB")
+    NPM_RUN_FORMAT=epub
+    ;;
+  "HTML")
+    NPM_RUN_FORMAT=html
+    ;;
+  *)
+    echo "Unknown format: $INPUT_FORMAT"
+    exit 1
+    ;;
+esac
 
 echo "Run Initialization and build step"
-cd $INPUT_TARGET_DIR && bundle install && npm install && REVIEW_CONFIG_FILE=$INPUT_CONFIG_FILE npm run pdf
+cd $INPUT_TARGET_DIR && bundle install && npm install && REVIEW_CONFIG_FILE=$INPUT_CONFIG_FILE npm run $NPM_RUN_FORMAT
 echo "Finish build step"
